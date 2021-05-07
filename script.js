d7393394b532ed004db9bbd2e103862799500fa7
Eric Kreis <erickreis793@gmail.com>
2021-05-07 18:32:16 -0300

    Requisito 02 e 03

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function showTextExec() {
   const input = document.getElementById('text-input');
   const text = document.getElementById('meme-text');
@@ -7,9 +6,18 @@ function showTextExec() {
 
 function showText() {
   const input = document.getElementById('text-input');
-  input.addEventListener('keyup', showTextExec)
+  input.addEventListener('keyup', showTextExec);
+}
+
+function showImage() {
+  const imgInput = document.getElementById('meme-insert');
+  const imgLocal = document.getElementById('meme-image');
+  imgInput.addEventListener('change', function() {
+    imgLocal.src = URL.createObjectURL(imgInput.files[0]);
+  });
 }
 
 window.onload = function memeGenerator() {
   showText();
+  showImage();
 };
