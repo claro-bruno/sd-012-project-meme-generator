6a0a4eefecdca8570d44d1461d668205bb5d9ea1
RyanLaiber <ryanlaiber@gmail.com>
2021-05-07 18:56:24 -0300

    adiciona requisito 2, faz upload de imagem

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let caminhoTextInput = document.getElementById("text-input");
+let caminhoImageInput = document.getElementById("meme-insert");
 let caminhoMemeContainer = document.getElementById("meme-image-container");
 
 //  função insere texto no meme-image-container
@@ -18,3 +19,21 @@ function insereTexto (){
 //  adiciona eventListener em text-input
 caminhoTextInput.addEventListener("input", insereTexto);
 
+//  função insere imagem em meme-image-container
+function insereImagem (){
+    let caminhoImagem = document.getElementById("meme-image");
+    let objetoFile = caminhoImageInput.files[0];
+    let imageUrl = URL.createObjectURL(objetoFile);
+
+    if(caminhoImagem){
+        caminhoImagem.src = imageUrl;
+    }else {
+        caminhoImagem = document.createElement("img");
+        caminhoImagem.setAttribute("id", "meme-image");
+        caminhoImagem.src = imageUrl;
+        caminhoMemeContainer.appendChild(caminhoImagem);
+    };
+};
+
+//  adiciona eventListener em text-input
+caminhoImageInput.addEventListener("change", insereImagem);
