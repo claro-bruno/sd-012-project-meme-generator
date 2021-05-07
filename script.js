75e3f98fc5d9c780eea48a9d9373cb584446f9d2
Eric Kreis <erickreis793@gmail.com>
2021-05-07 20:01:24 -0300

    Requisitos Bonus :smile:

--- script.js
+++ script.js
@@ -9,15 +9,56 @@ function showText() {
   input.addEventListener('keyup', showTextExec);
 }
 
-function showImage() {
+function showImageClick() {
   const imgInput = document.getElementById('meme-insert');
   const imgLocal = document.getElementById('meme-image');
-  imgInput.addEventListener('change', function() {
-    imgLocal.src = URL.createObjectURL(imgInput.files[0]);
+  imgLocal.src = URL.createObjectURL(imgInput.files[0]);
+}
+
+function showImage() {
+  const imgInput = document.getElementById('meme-insert');
+  imgInput.addEventListener('change', showImageClick);
+}
+
+function styleButtons() {
+  const fire = document.getElementById('fire');
+  const water = document.getElementById('water');
+  const earth = document.getElementById('earth');
+  const container = document.getElementById('meme-image-container');
+  fire.addEventListener('click', () => {
+    container.style.border = '3px dashed red';
+  });
+  water.addEventListener('click', () => {
+    container.style.border = '5px double blue';
+  });
+  earth.addEventListener('click', () => {
+    container.style.border = '6px groove green';
+  });
+}
+
+function addFamousImgs() {
+  const meme1 = document.getElementById('meme-1');
+  const meme2 = document.getElementById('meme-2');
+  const meme3 = document.getElementById('meme-3');
+  const meme4 = document.getElementById('meme-4');
+  const imgLocal = document.getElementById('meme-image');
+  meme1.addEventListener('click', () => {
+    imgLocal.src = 'imgs/meme1.png';
+  });
+  meme2.addEventListener('click', () => {
+    imgLocal.src = 'imgs/meme2.png';
+  });
+  meme3.addEventListener('click', () => {
+    imgLocal.src = 'imgs/meme3.png';
+  });
+  meme4.addEventListener('click', () => {
+    imgLocal.src = 'imgs/meme4.png';
   });
 }
 
 window.onload = function memeGenerator() {
   showText();
   showImage();
+  styleButtons();
+  addFamousImgs();
 };
