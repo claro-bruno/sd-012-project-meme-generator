5247573a3960602d0467f271320fae7074b77b31
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-07 18:23:25 -0300

    adiciona requisito 2

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let inputTexto = document.querySelector('#text-input');
-let textoMeme = document.querySelector('#meme-text');
+const inputTexto = document.querySelector('#text-input');
+const inputImg = document.querySelector('#meme-insert');
+const textoMeme = document.querySelector('#meme-text');
+let imgMeme = document.querySelector('#meme-image');
 
 function addTexto() {
     if (inputTexto.value.length > 0) {
@@ -8,3 +10,11 @@ function addTexto() {
 }
 
 inputTexto.addEventListener('keyup', addTexto);
+
+function addImagem() {
+    if (this.files.length >0) {
+        imgMeme.src = URL.createObjectURL(this.files[0]);
+    }
+}
+
+inputImg.addEventListener('change', addImagem);
