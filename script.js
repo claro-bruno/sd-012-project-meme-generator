d6335fda9944d1aa0785d88ac23656d9ecd47222
Eric Faria <ericdeofaria@gmail.com>
2021-05-07 18:44:41 -0300

    Requisito 06

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const txtInput = document.getElementById('text-input');
 const imgInput = document.getElementById('meme-insert');
 const memeTxt = document.getElementById('meme-text');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+const container = document.getElementById('meme-image-container');
 
 function textUpdate() {
   memeTxt.innerText = txtInput.value;
@@ -11,7 +15,23 @@ function imageUpdate() {
   img.src = URL.createObjectURL(imgInput.files[0]);
 }
 
+function fire() {
+  container.style.border = '3px dashed red';
+}
+
+function water() {
+  container.style.border = '5px double blue';
+}
+
+function earth() {
+  container.style.border = '6px groove green';
+}
+
 window.onload = function pageLoad() {
   txtInput.addEventListener('keyup', textUpdate);
   imgInput.addEventListener('change', imageUpdate);
+  
+  btnFire.addEventListener('click', fire);
+  btnWater.addEventListener('click', water);
+  btnEarth.addEventListener('click', earth);
 };
