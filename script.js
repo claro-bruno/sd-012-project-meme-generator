ea2acb004217946483c128e7c28d69dcbb953ab8
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-08 17:16:49 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const inputText = document.querySelector('#text-input');
-// const container = document.querySelector('#meme-image-container');
+const container = document.querySelector('#meme-image-container');
 const textImage = document.querySelector('#meme-text');
 const fileSelector = document.querySelector('#meme-insert');
 const imageDisplay = document.querySelector('#meme-image');
+const allButtons = document.querySelectorAll('button');
 
 inputText.addEventListener('keyup', (event) => {
   textImage.innerHTML = event.target.value;
@@ -11,3 +12,13 @@ inputText.addEventListener('keyup', (event) => {
 fileSelector.addEventListener('change', (event) => {
   imageDisplay.src = URL.createObjectURL(event.target.files[0]);
 });
+
+function differentBorders() {
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', (event) => {
+      container.classList = event.target.innerHTML.toLowerCase();
+    });
+  }
+}
+
+differentBorders();
