02bc3c953075874aee6b48a290c69afe50c5bd2d
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-07 22:34:28 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ function setMemeText() {
   getMemeText.innerText = getTextInput;
 }
 
+function setMemeImg(event) {
+  let getMemeImage = document.getElementById('meme-image');
+  let urlImg = URL.createObjectURL(event.target.files[0]);
+  getMemeImage.src = urlImg;
+  getMemeImage.className = 'input-image';
+}
+
 document.addEventListener('keyup', (event) => {
-  if (event.target.id === 'text-input')
-  setMemeText();
+  if (event.target.id === 'text-input') {
+    setMemeText();
+  }
 });
+
+const getFileInput = document.getElementById('meme-insert');
+getFileInput.addEventListener('change', (event) => {
+  setMemeImg(event);
+})
