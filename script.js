c992c2ef20180b1afc2444253d179d766294688b
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-08 16:43:01 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const inputText = document.querySelector('#text-input');
 // const container = document.querySelector('#meme-image-container');
 const textImage = document.querySelector('#meme-text');
+const fileSelector = document.querySelector('#meme-insert');
+const imageDisplay = document.querySelector('#meme-image');
 
 inputText.addEventListener('keyup', (event) => {
   textImage.innerHTML = event.target.value;
+});
+
+fileSelector.addEventListener('change', (event) => {
+  imageDisplay.src = URL.createObjectURL(event.target.files[0]);
 });
