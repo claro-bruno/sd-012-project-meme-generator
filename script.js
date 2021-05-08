ed1d039bc1bcd3a04896f971d488c88ba363313d
Eduardo Linhares <modsgtasagame@gmail.com>
2021-05-07 21:13:57 -0300

    Resolve ESlint

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let getMemeContainer;
-let image;
+let imagem;
 let memeInsert;
 
-function carregarArquivo() {
-  memeInsert.addEventListener('change', (event) => {
-    image.src = URL.createObjectURL(event.target.files[0]);
-  })
+function carregarArquivo(event = null) {
+  if (event !== null) {
+    imagem.src = URL.createObjectURL(event.target.files[0]);
+    memeInsert.value = '';
+  }
 }
 
 function digitarTextoMeme() {
@@ -13,49 +14,47 @@ function digitarTextoMeme() {
   const getText = document.getElementById('meme-text');
   getInput.addEventListener('keyup', () => {
     getText.innerHTML = getInput.value;
-  })
+  });
 }
 
 function fire() {
   const getButtonFire = document.getElementById('fire');
   getButtonFire.addEventListener('click', () => {
     getMemeContainer.style.border = '3px dashed red';
-  })
+  });
 }
 
 function water() {
   const getButtonFire = document.getElementById('water');
   getButtonFire.addEventListener('click', () => {
     getMemeContainer.style.border = '5px double blue';
-  })
+  });
 }
 
 function earth() {
   const getButtonFire = document.getElementById('earth');
   getButtonFire.addEventListener('click', () => {
     getMemeContainer.style.border = '6px groove green';
-  })
+  });
 }
 
 function memes() {
-  const modelos = document.getElementsByClassName('model');
-  for (let i = 0; i < modelos.length; i += 1) {
-    modelos[i].addEventListener('click', (event) => {
-      image.src = event.target.src;
-    })
-  }
+  const todosModelos = document.getElementById('models');
+  todosModelos.addEventListener('click', (event) => {
+    if (event.target.className.includes('model')) {
+      imagem.src = event.target.src;
+    }
+  });
 }
 
-window.onload = () => {
-
+window.onload = function loader() {
   getMemeContainer = document.getElementById('meme-image-container');
-  image = document.getElementById('meme-image');
+  imagem = document.getElementById('meme-image');
   memeInsert = document.getElementById('meme-insert');
-
   digitarTextoMeme();
-  carregarArquivo();
   fire();
   water();
   earth();
   memes();
-}
+  carregarArquivo();
+};
