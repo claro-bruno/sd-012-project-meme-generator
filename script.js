f6da60403b9e5fd873926d471f0ffaa0ba54d4f9
Patrick Dack <patrickdack89@gmail.com>
2021-05-08 17:41:15 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const file = document.querySelector('#meme-insert');
-function loadFile (event) {
+function loadFile(event) {
   const image = document.querySelector('#meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 }
@@ -7,7 +7,21 @@ file.addEventListener('change', loadFile);
 
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-function insertText () {
+function insertText() {
   memeText.innerText = textInput.value;
 }
 textInput.addEventListener('keyup', insertText);
+
+const btnFire = document.querySelector('#fire');
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
+const container = document.querySelector('#meme-image-container')
+btnFire.addEventListener('click', () => {
+  container.style.border = '3px dashed red';
+});
+btnWater.addEventListener('click', () => {
+  container.style.border = '5px double blue';
+});
+btnEarth.addEventListener('click', () =>{
+  container.style.border = '6px groove green';
+});
