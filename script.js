2cfb5ad8e05fce2e0ede54cf60872f4fc179e72b
Patrick Dack <patrickdack89@gmail.com>
2021-05-08 16:24:02 -0300

    Faz adiçoes para o requito 1 e 2

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const file = document.querySelector('#meme-insert');
-
 function loadFile (event) {
   const image = document.querySelector('#meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 }
 file.addEventListener('change', loadFile);
+
+const textInput = document.querySelector('#text-input');
+const memeText = document.querySelector('.meme-text');
+function insertText () {
+  memeText.innerText = textInput.value;
+}
+textInput.addEventListener('keyup', insertText);
