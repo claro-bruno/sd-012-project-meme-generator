ad9cd8d5ca49da6a1ee6cfc630521fc73b072b85
Roberval Filho <rslfilho@gmail.com>
2021-05-09 10:13:35 -0300

    R1 - Resolvido

---
+++ script.js
@@ -0,0 +1,10 @@
+function getText() {
+  const textInput = document.getElementById('text-input');
+  const memeText = document.getElementById('meme-text');
+
+  textInput.addEventListener('keyup', (event) => {
+    memeText.innerHTML = event.target.value;
+  });
+}
+
+getText();
