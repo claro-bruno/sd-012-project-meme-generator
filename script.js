1fc71b301252c931fd96e61b697855aa0571775a
Roberval Filho <rslfilho@gmail.com>
2021-05-09 15:26:00 -0300

    R6 - Resolvido

--- script.js
+++ script.js
@@ -19,5 +19,32 @@ function getImage() {
   });
 }
 
+function createCustomButton(id, text) {
+  const optionsContainer = document.getElementById('options');
+  const customButton = document.createElement('button');
+
+  customButton.className = 'custom-button';
+  customButton.id = id;
+  customButton.innerHTML = text;
+
+  optionsContainer.appendChild(customButton);
+}
+
+function customizeButton() {
+  const buttons = document.querySelectorAll('.custom-button');
+  const buttonBorder = ['3px dashed red', '5px double blue', '6px groove green'];
+  const memeContainer = document.getElementById('meme-image-container');
+
+  for (let index = 0; index < buttons.length; index += 1) {
+    buttons[index].addEventListener('click', () => {
+      memeContainer.style.border = buttonBorder[index];
+    });
+  }
+}
+
 getText();
 getImage();
+createCustomButton('fire', 'Fire &#128293');
+createCustomButton('water', 'Water &#128167');
+createCustomButton('earth', 'Earth \u26f0');
+customizeButton();
