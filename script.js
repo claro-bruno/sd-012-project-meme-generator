5484e6475995ed356ddbd4ad2f1f86f3ad6c880c
Roberval Filho <rslfilho@gmail.com>
2021-05-09 11:02:57 -0300

    R2 - Resolvido

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const imageInput = document.getElementById('meme-insert');
+const img = document.getElementById('meme-image');
+
 function getText() {
   const textInput = document.getElementById('text-input');
   const memeText = document.getElementById('meme-text');
@@ -7,4 +10,14 @@ function getText() {
   });
 }
 
+function getImage() {
+  imageInput.addEventListener('change', (event) => {
+    img.src = URL.createObjectURL(event.target.files[0]);
+    img.onload = () => {
+      URL.revokeObjectURL(event);
+    };
+  });
+}
+
 getText();
+getImage();
