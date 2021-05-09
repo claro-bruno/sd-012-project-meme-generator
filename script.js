252c482efa239c8b1da739374c7c9f6f03f04498
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-09 00:09:10 -0300

    muda declaracao das funcoes

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const memeText = document.getElementById('meme-text');
 const memeImg = document.getElementById('meme-image');
 const imgInput = document.getElementById('meme-insert');
 
-textInput.addEventListener('keyup', addText = () => {
+function addText() {
   memeText.innerHTML = textInput.value;
-});
+}
 
-imgInput.addEventListener('change', displayImg = (event) => {
+function displayImg(event) {
   memeImg.src = URL.createObjectURL(event.target.files[0]); // do artigo https://www.webtrickshome.com/faq/how-to-display-uploaded-image-in-html-using-javascript
-});
+}
+
+textInput.addEventListener('keyup', addText);
+
+imgInput.addEventListener('change', displayImg);
