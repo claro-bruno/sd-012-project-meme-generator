f630dfd3224cfead411a219b85cc68c74f0b2610
GMODias <gmd.delta@gmail.com>
2021-05-10 11:22:26 -0300

    [PT-BR]Atendendo ate o requisito 5. [EN] Solved until 5th requirement.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const memeTextplace = document.getElementById('meme-text');
 const inputText = document.getElementById('text-input');
+const memeImgplace = document.getElementById('meme-image');
+const inputImg = document.getElementById('meme-insert');
 
 // Pegar texto digitado para a seção de criação de meme.
 function getText() {
@@ -7,3 +9,10 @@ function getText() {
 }
 
 inputText.addEventListener('keyup', getText);
+
+// Pegar imagem selecionada e exibir
+function getImg(event) {
+  memeImgplace.src = URL.createObjectURL(event.target.files[0]);
+}
+
+inputImg.onchange = getImg;
