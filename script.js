1457a3061a71157cc935f2b4ee7cf9b66031d129
GMODias <gmd.delta@gmail.com>
2021-05-10 11:54:27 -0300

    [PT-BR]Atendendo requisito 6. [EN] Solved 6th requirement.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const memeTextplace = document.getElementById('meme-text');
 const inputText = document.getElementById('text-input');
 const memeImgplace = document.getElementById('meme-image');
 const inputImg = document.getElementById('meme-insert');
+const marginChangers = document.getElementById('marginsBtns');
+const memeContainer = document.getElementById('meme-image-container');
 
 // Pegar texto digitado para a seção de criação de meme.
 function getText() {
@@ -16,3 +18,24 @@ function getImg(event) {
 }
 
 inputImg.onchange = getImg;
+
+// Mudar margens
+function changeMargin(event) {
+  if (event.target.tagName !== 'SECTION') {
+    switch (event.target.id) {
+    case 'fire':
+      memeContainer.style.border = '3px dashed red';
+      break;
+    case 'water':
+      memeContainer.style.border = '5px double blue';
+      break;
+    case 'earth':
+      memeContainer.style.border = '6px groove green';
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+marginChangers.addEventListener('click', changeMargin);
