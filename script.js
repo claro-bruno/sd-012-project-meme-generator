6b0d2b21e7a9beb1c4fdf837c39384dfd4231cca
GMODias <gmd.delta@gmail.com>
2021-05-10 13:03:58 -0300

    [PT-BR]Atendendo requisito 7. [EN] Solved 7th requirement.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const memeImgplace = document.getElementById('meme-image');
 const inputImg = document.getElementById('meme-insert');
 const marginChangers = document.getElementById('marginsBtns');
 const memeContainer = document.getElementById('meme-image-container');
+const miniPlace = document.getElementById('miniatures');
 
 // Pegar texto digitado para a seção de criação de meme.
 function getText() {
@@ -39,3 +40,26 @@ function changeMargin(event) {
 }
 
 marginChangers.addEventListener('click', changeMargin);
+
+// Cria imagens pequenas abaixo como opção de seleção
+function geraImagens() {
+  for (let index = 1; index <= 4; index += 1) {
+    const miniImg = document.createElement('img');
+    miniImg.src = `/imgs/meme${index}.png`;
+    miniImg.className = 'mini';
+    miniImg.id = `meme-${index}`;
+    console.log(miniImg);
+    miniPlace.appendChild(miniImg);
+  }
+}
+
+window.onload = geraImagens;
+
+// Colocar a miniatura selecionada e colocar em exibição
+function getMini(event) {
+  if (event.target.tagName !== 'SECTION') {
+    memeImgplace.src = event.target.src;
+  }
+}
+
+miniPlace.addEventListener('click', getMini);
