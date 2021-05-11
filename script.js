93b3fcf115ddd70ad0666710fe821164973af2a2
José Carlos <kalarruda@hotmail.com>
2021-05-11 12:53:44 -0300

    verifica erros no console

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let inputText = document.getElementById('text-input');
-let containerImagem = document.getElementById('meme-image-container');
-let memeTexto = document.getElementById('meme-text');
+const inputText = document.getElementById('text-input');
+const containerImagem = document.getElementById('meme-image-container');
+const memeTexto = document.getElementById('meme-text');
 containerImagem.appendChild(memeTexto);
+const btnInsert = document.getElementById('meme-insert"');
+// btnInsert.innerHTML = 'Choose File';
 
-function AddText() {  
-  memeTexto.innerHTML = inputText.value;  
+function AddText() {
+  memeTexto.innerHTML = inputText.value;
 }
 
 inputText.addEventListener('keyup', AddText);
+
+let imagem = document.getElementById('meme-image');
+
+let loadImage = function (event) {
+  imagem.src = URL.createObjectURL(event.target.files[0]);
+}
+
 
