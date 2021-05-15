12da8711c5558013b34e18b5d7f2e7c2dce22d11
Jossany Moura <mjossany@gmail.com>
2021-05-15 12:10:32 -0300

    Finaliza requisito 3

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ catchTextInput.addEventListener('keyup', function(event) {
   catchLegendElement.innerHTML = event.target.value;
 });
 
-//Captura a imagem selecionada e imprime na tela
+//Captura a imagem selecionada e imprime na tela. Solução retirada do repositório do meu colega de trybe Roberval Filho https://github.com/tryber/sd-012-project-meme-generator/pull/17/commits/5484e6475995ed356ddbd4ad2f1f86f3ad6c880c
 catchImageInput.addEventListener('change', function(event) {
   catchImage.src = URL.createObjectURL(event.target.files[0]);
   catchImage.onload = function() {
     URL.revokeObjectURL(event);
   }
 })
+
