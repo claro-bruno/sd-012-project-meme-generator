3fad957446b87370e6751dc2af3e4f57159cb9bf
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-05-16 02:38:44 -0300

    Resolve o requisito 2

--- script.js
+++ script.js
@@ -2,14 +2,27 @@
 let userText = document.getElementById('text-input');
 let imageTextDiv = document.getElementById('meme-image-container');
 let textDiv = document.getElementById('meme-text');
+let userImage = document.getElementById('meme-insert');
+let showImage = document.getElementById('show-image');
 
 
 
 
-userText.addEventListener('keyup', function(){
-  if (userText != null) {      
+userText.addEventListener('keyup', function(){ //adiciona texto
+  if (userText) {      
     textDiv.innerHTML = userText.value;    
   }
 })
+
+
+
+userImage.addEventListener('change', function(event){
+  showImage.src = URL.createObjectURL(event.target.files[0]);
+  })
+
+
+
+
+
 
 
