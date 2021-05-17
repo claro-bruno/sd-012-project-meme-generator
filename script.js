0f58f250305dea42ae749557f37a7fdc88986eaf
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-05-17 01:57:57 -0300

    Resolve o requisito 7

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ let userText = document.getElementById('text-input');
 let imageTextDiv = document.getElementById('meme-image-container');
 let textDiv = document.getElementById('meme-text');
 let userImage = document.getElementById('meme-insert');
-let showImage = document.getElementById('show-image');
+let showImage = document.getElementById('meme-image');
 let fire = document.getElementById('fire');
 let water = document.getElementById('water');
+let meme1 = document.getElementById('meme-1');
+let meme2 = document.getElementById('meme-2');
+let meme3 = document.getElementById('meme-3');
+let meme4 = document.getElementById('meme-4');
+
+
 
 
 
@@ -33,6 +39,23 @@ water.addEventListener('click', function(){
 earth.addEventListener('click', function(){  
   imageTextDiv.style.border= '6px groove green';  
 })
+
+meme1.addEventListener('click', function(){
+  showImage.src = "imgs/meme1.png";
+})
+
+meme2.addEventListener('click', function(){
+  showImage.src = "imgs/meme2.png";
+})
+
+meme3.addEventListener('click', function(){
+  showImage.src = "imgs/meme3.png";
+})
+
+meme4.addEventListener('click', function(){
+  showImage.src = "imgs/meme4.png";
+})
+
 
 
 
