23a3c217536f82d0d3256d1f67243db225066e74
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-05-17 01:17:06 -0300

    Resolve o requisito 6

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let imageTextDiv = document.getElementById('meme-image-container');
 let textDiv = document.getElementById('meme-text');
 let userImage = document.getElementById('meme-insert');
 let showImage = document.getElementById('show-image');
+let fire = document.getElementById('fire');
+let water = document.getElementById('water');
 
 
 
@@ -16,10 +18,21 @@ userText.addEventListener('keyup', function(){ //adiciona texto
 
 
 
-userImage.addEventListener('change', function(event){
+userImage.addEventListener('change', function(event){ //adiciona imagem
   showImage.src = URL.createObjectURL(event.target.files[0]);
   })
 
+fire.addEventListener('click', function(){
+  imageTextDiv.style.border= '3px dashed red';  
+})
+
+water.addEventListener('click', function(){  
+  imageTextDiv.style.border= '5px double blue';  
+})
+
+earth.addEventListener('click', function(){  
+  imageTextDiv.style.border= '6px groove green';  
+})
 
 
 
