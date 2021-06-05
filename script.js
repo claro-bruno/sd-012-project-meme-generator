39ad9ee61a77b396d7af4be366c71b3d9254b2c7
Nuwanda <morato_dev@protonmail.com>
2021-06-05 13:46:27 -0300

    botoes e começo de estilizaçao

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ imageSubtitle();
 
 const imgBackground = () => {
   imageInput.addEventListener('change', (evt) => {
+    // credit goes to https://stackoverflow.com/questions/31353703/how-to-upload-image-file-from-computer-and-set-as-div-background-image-using-jqu
     const imageOutput = document.getElementById('meme-image');
     const imageAsURL = URL.createObjectURL(evt.target.files[0]);
     imageOutput.style.backgroundImage = `url(${imageAsURL})`;
